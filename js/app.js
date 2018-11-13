48bd5230ab16b177fabfccf7e7361bcf1015c2f7
RobertoRosa7 <roberto.rosa7@gmail.com>
2018-11-13 16:30:41 -0200

    validação de formulário finalizado

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 * Criando a classe de despesa para o Objeto
 * Recuperando os dados do formulário para inserção no atributos do objeto
 */
+
+const animateClass = 'animate';
+
 class Despesas{
 	constructor(ano,mes,dia,tipo,descricao,valor){
 		this.ano = ano;
@@ -10,6 +13,76 @@ class Despesas{
 		this.tipo = tipo;
 		this.descricao = descricao;
 		this.valor = valor;
+	}
+	validarDados(){
+		/* necessário recuperar todos os dados do formulário, percorrendo cada um
+		* para percorrer os valores dos atruibuto da classe, deve usar this[i], assim o this que faz referência
+		* a este objeto será percorrido os seus valores.
+		*/
+		
+		if(this.ano < 2018){
+			var i = 5;
+			var x = setInterval(function(){
+				document.getElementById('notify-ano').className = animateClass;
+				i--;
+				if(i === 0){
+					document.getElementById('notify-ano').className = 'opacity-0';
+					clearInterval(x);
+				}
+			},1000)
+			
+		}else if(this.mes == undefined || this.mes == '' || this.mes == null){
+			var i = 5;
+			var x = setInterval(function(){
+				document.getElementById('notify-mes').className = animateClass;
+				i--;
+				if(i === 0){
+					document.getElementById('notify-mes').className = 'opacity-0';
+					clearInterval(x);
+				}
+			},1000)
+
+		}else if(this.dia == undefined || this.dia == '' || this.dia == null){
+			var i = 5;
+			var x = setInterval(function(){
+				document.getElementById('notify-dia').className = animateClass;
+				i--;
+				if(i === 0){
+					document.getElementById('notify-dia').className = 'opacity-0';
+					clearInterval(x);
+				}
+			},1000)
+		}else if(this.tipo == undefined || this.tipo == '' || this.tipo == null){
+			var i = 5;
+			var x = setInterval(function(){
+				document.getElementById('notify-tipo').className = animateClass;
+				i--;
+				if(i === 0){
+					document.getElementById('notify-tipo').className = 'opacity-0';
+					clearInterval(x);
+				}
+			},1000)
+		}else if(this.valor == undefined || this.valor == '' || this.valor == null){
+			var i = 5;
+			var x = setInterval(function(){
+				document.getElementById('notify-valor').className = animateClass;
+				i--;
+				if(i === 0){
+					document.getElementById('notify-valor').className = 'opacity-0';
+					clearInterval(x);
+				}
+			},1000)
+		} else {
+				for(let i in this){
+				//debug
+				//console.log(i, this[i])
+				if(this[i] == undefined || this[i] == '' || this[i] == null){
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 }
 class Db{
@@ -70,7 +143,13 @@ function cadastrarDespesas(){
 	//Instanciação o Objeto com os parâmetro
 	let despesa = new Despesas(ano.value,mes.value,dia.value,tipo.value,descricao.value,valor.value);
 
-	//Debug
-	//console.log(despesa);
-	db.gravar(despesa);
+	if(despesa.validarDados()){
+		//Debug
+		//console.log(despesa);
+		//db.gravar(despesa);
+		console.log('dados invalidos');
+	}else{
+		//dialog de erro
+		console.log('dados validos');
+	}
 }
